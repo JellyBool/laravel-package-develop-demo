4f757a3e0a12ddcbe360986fcc3eb5b1c040314e
JellyBool <jellybool@outlook.com>
2016-09-20 15:03:54 +0800

    init project' ;

---
+++ src/MD5Hasher.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Laravist\Hasher;
+
+/**
+ * Class MD5Hasher
+ *
+ * @package Laravist\Hasher
+ */
+class MD5Hasher
+{
+    /**
+     * @param $value
+     * @param array $options
+     * @return string
+     */
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return hash('md5', $value . $salt);
+    }
+
+    /**
+     * @param $value
+     * @param $hashValue
+     * @param array $options
+     * @return bool
+     */
+    public function check($value, $hashValue, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+
+        return hash('md5', $value . $salt) === $hashValue;
+    }
+}
